c909737a499d3b7d0786c7faedb90b0a83712556
Mateusz Skrok <m.p.skrok@gmail.com>
2020-05-04 21:59:58 +0200

    Add transpose

--- src/components/Song.js
+++ src/components/Song.js
@@ -11,6 +11,85 @@ import TapTempoButton from "./TapTempoButton";
 import ChordsAPI from "../api/ChordsAPI";
 import NotesAPI from "../api/NotesAPI";
 
+function Transpose(props){
+    
+    console.log(props.chords)
+    let splittedChords = props.chords.toString().split(" ");
+    const transposeTable = [
+        {key: "c", value: 0.5},
+        {key: "cis", value: 1.5},
+        {key: "des", value: 1.5},
+        {key: "d", value: 2.5},
+        {key: "dis", value: 3.5},
+        {key: "es", value: 3.5},
+        {key: "e", value: 4.5},
+        {key: "f", value: 5.5}, 
+        {key: "fis", value: 6.5},
+        {key: "ges", value: 6.5},
+        {key: "g", value: 7.5},
+        {key: "gis", value:  8.5},
+        {key: "as", value: 8.5},
+        {key: "a", value:  9.5},
+        {key: "ais",value:  10.5},
+        {key: "b", value: 10.5},
+        {key: "h", value: 11.5},
+
+        {key: "C", value: 0},
+        {key: "Cis", value: 1},
+        {key: "Des", value: 1},
+        {key: "D", value: 2},
+        {key: "Dis", value: 3},
+        {key: "Es", value: 3},
+        {key: "E", value: 4},
+        {key: "F", value: 5}, 
+        {key: "Fis", value: 6},
+        {key: "Ges", value: 6},
+        {key: "G", value: 7},
+        {key: "Gis", value:  8},
+        {key: "As", value: 8},
+        {key: "A", value:  9},
+        {key: "Ais",value:  10},
+        {key: "B", value: 10},
+        {key: "H", value: 11} 
+    ]
+    const decodeTable = [
+        {key: "c", value: 0.5},
+        {key: "cis", value: 1.5},
+        {key: "d", value: 2.5},
+        {key: "dis", value: 3.5},
+        {key: "e", value: 4.5},
+        {key: "f", value: 5.5}, 
+        {key: "fis", value: 6.5},
+        {key: "g", value: 7.5},
+        {key: "gis", value:  8.5},
+        {key: "a", value:  9.5},
+        {key: "ais",value:  10.5},
+        {key: "h", value: 11.5},
+
+        {key: "C", value: 0},
+        {key: "Cis", value: 1},
+        {key: "D", value: 2},
+        {key: "Dis", value: 3},
+        {key: "E", value: 4},
+        {key: "F", value: 5}, 
+        {key: "Fis", value: 6},
+        {key: "G", value: 7},
+        {key: "Gis", value:  8},
+        {key: "A", value:  9},
+        {key: "Ais",value:  10},
+        {key: "H", value: 11} 
+    ]
+    console.log(splittedChords)
+    const transposedChordIndexes = splittedChords.map(
+        chord => (transposeTable.find(a => a.key===chord).value + props.semitones) % 12
+    )
+    const transposedChords = transposedChordIndexes.map(
+     index => decodeTable.find(a => a.value===index).key + " "
+    )
+      
+    return transposedChords;
+}
+
 class Song extends React.Component{
 
     state={
@@ -19,7 +98,8 @@ class Song extends React.Component{
         areLyricsVisible: true,
         areNotesVisible: false,
         areChordSVisible: false,
-        error: null
+        error: null,
+        transpose:0
     };
 
     componentDidMount(){
@@ -42,6 +122,23 @@ class Song extends React.Component{
         )
     }
 
+    handleTransposeDown = () => {
+        this.setState((prevState) => {
+            return{
+                transpose: prevState.transpose-1}
+        })
+    }
+
+    handleTransposeUp= () => {
+        this.setState((prevState) => {
+            return{
+                transpose: prevState.transpose+1}
+        })
+    }
+    
+    handleTransposeReset = () => {
+        this.setState({transpose:0})
+    }
 
     handleToggleNotesVisibility = (event) => {
         this.setState((prevState) => {
@@ -119,6 +216,15 @@ class Song extends React.Component{
                             <Box as={GiMusicalScore}/>
                         </Button>
                         <TapTempoButton tempo={tempo}/>
+                        <Button onClick={this.handleTransposeDown}>
+                            -
+                        </Button>
+
+                        <Button onDoubleClick={this.handleTransposeReset}>TRANSPOSE: {this.state.transpose} </Button>
+                        
+                        <Button onClick={this.handleTransposeUp}>
+                            +
+                        </Button>
                     </ButtonGroup>
                     
                     {areNotesVisible ?
@@ -129,7 +235,7 @@ class Song extends React.Component{
                     : ""}
                     {areChordsVisible ? 
                     <Box>
-                        <Text as="kbd">{chords}</Text>
+                        <Text as="kbd"><Transpose chords={chords} semitones={this.state.transpose}></Transpose></Text>
                     </Box>
                     : ""}
                     
