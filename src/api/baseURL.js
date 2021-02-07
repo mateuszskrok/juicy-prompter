3d6694482a026b33c7561fb3b9483c6de4982781
Mateusz Skrok <m.p.skrok@gmail.com>
2021-02-07 01:16:39 +0100

    bugfix

--- src/api/baseURL.js
+++ src/api/baseURL.js
@@ -1,5 +1,5 @@
 //const BASE_URL = ("http://localhost:4000")
 //const BASE_URL = ("http://192.168.0.103:4000")
-const BASE_URL = ("https://juicy-prompter2-server.herokuapp.com/")
+const BASE_URL = ("https://juicy-prompter2-server.herokuapp.com")
 
 export default BASE_URL;
